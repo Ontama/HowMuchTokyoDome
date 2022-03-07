e2cc1d7e5a55e451d5333b4ae3cfc655bcfd5723
ontama154 <onchanvvv@gmail.com>
2022-03-07 12:38:13 +0900

    Initial Commit

---
+++ Shared/HowMuchTokyoDomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowMuchTokyoDomeApp.swift
+//  Shared
+//
+//  Created by tomoyo_kageyama on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct HowMuchTokyoDomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
