b9fa01c513a920509f8fc14c5e65a068accc2f34
ontama154 <onchanvvv@gmail.com>
2022-03-24 15:22:51 +0900

    AppDelegate,LocationParameterをクラスとして切り出し

--- Shared/View/MapView.swift
+++ Shared/View/MapView.swift
@@ -37,7 +37,6 @@ struct MapView: View {
 //            .onReceive(viewModel.$location) { locations in
 //                updateReigion(coordinate: CLLocationCoordinate2D(latitude: viewModel.latitude, longitude: viewModel.longitude))
 //            }
-            
     }
     
     // 引数で取得した緯度経度を使って動的に表示領域の中心位置と、縮尺を決める
